da82c711f165c3154d403f9cbe39b1bb5e1dd738
Destroier650 <alefemanuelmv@gmail.com>
2025-10-06 16:11:26 -0300

    10/06/2025

---
+++ competitive-programming/CodeForces/bearAndBigBrother.py
@@ -0,0 +1,10 @@
+x, y = map(int, input().split())
+
+a = 0
+
+while x <= y:
+    x *= 3
+    y *= 2
+    a += 1
+
+print(a)
---
+++ competitive-programming/CodeForces/boyOrGirl.py
@@ -0,0 +1,5 @@
+x = input()
+if len(set(x)) % 2 != 0:
+    print("IGNORE HIM!")
+else:
+    print("CHAT WITH HER!")
